2bb645900197134bfe19640dd8c7f6a424fab257
kenkomkom <kenichi@gmail.com>
2023-09-08 20:00:21 +0700

    a

--- main/views.py
+++ main/views.py
@@ -4,7 +4,8 @@ from .models import Items
 # Create your views here.
 def show_cards(request):
     all_card = Items.objects.all().values()
+    
     response = {'cards': all_card}
-    # print(all_card)
+    print(all_card)
     
     return render(request, 'show_cards.html', response)
