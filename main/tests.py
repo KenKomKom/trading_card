637f43aeb0d33c86c6a7916daf55c79ef0ee9f79
KenKomKom <kenichikomala@gmail.com>
2023-09-09 10:11:38 +0700

    a

--- main/tests.py
+++ main/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase, Client
 
-# Create your tests here.
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
-        response = Client().get('main')
+        response = Client().get('/main/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_url_is_exist(self):
+        response = Client().get('/admin/')
         self.assertEqual(response.status_code, 200)
 
     def test_main_using_main_template(self):
